3735c8321cba90034641c4f85e486a9c3853ae7a
irhsrykhan01 <irhasraykhan1@gmail.com>
2026-07-13 15:19:30 +0700

    Add new utility exports for Jid, Validator, and Message

--- src/lib/index.js
+++ src/lib/index.js
@@ -3,3 +3,6 @@ export { default as UrlUtil } from "./UrlUtil.js";
 export { default as FileUtil } from "./FileUtil.js";
 export { default as StringUtil } from "./StringUtil.js";
 export { default as TimeUtil } from "./TimeUtil.js";
+export { default as JidUtil } from "./JidUtil.js";
+export { default as ValidatorUtil } from "./ValidatorUtil.js";
+export { default as MessageUtil } from "./MessageUtil.js";
